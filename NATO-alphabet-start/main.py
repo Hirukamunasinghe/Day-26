88151ee68fe88ee1870d432145608e349710a1c4
Hirukamunasinghe <munasinghehiruka@gmail.com>
2022-03-09 22:11:58 +0530

    -

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -5,16 +5,23 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 #Create a dictionary
 #{new_key:new_value for (index,row) in df.iterrows()}
 phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
+print(phonetic_dict)
 #print(phonetic_dict)
 
 
 #Create a list of the phonetic code words from a word that the user inputs
-import sys
-try:
-    word = input("Enter a word: ").upper()
-except:
-    print("Please enter characters!")
-    sys.exit()
+
+
+
 #new_list = [new_item for item in list if test]
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet are allowed")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
